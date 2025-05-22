b4231d41a2653a7b1d4bf519a480edbf601b7f0e
giannilmbd <giannilmbd@gmail.com>
2025-05-22 17:53:05 +0200

    recompiled everything

--- compile_again_and_push.r
+++ compile_again_and_push.r
@@ -3,5 +3,5 @@
 system('rm src/*.so')
 Rcpp::compileAttributes(); devtools::document();devtools::build()
 system('git add .;git commit -m "recompiled everything"; git push origin master')
-system('cd ../only_tar;mv ../APRScenario_0.0.3.0.tar.gz; git add -f APRScenario_0.0.3.0.tar.gz; git commit -m "added new tarball"; git push origin tar-package')
+system('cd ../only_tar;mv ../APRScenario_0.0.3.0.tar.gz .; git add -f APRScenario_0.0.3.0.tar.gz; git commit -m "added new tarball"; git push origin tar-package')
 print("done")
